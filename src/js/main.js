483f85b5c016632ff32d7ce6475ce4b8cb2f5c2e
Abiloye <iabiloye@gmail.com>
2026-01-16 13:28:51 +0100

    Step 1234 covered

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+// This willload header and footer
+loadHeaderFooter();
+
+// product listing for tents    
+const listElement = document.querySelector(".product-list");
+if (listElement) {
+  const tentsList = new ProductList("tents", listElement);
+  tentsList.init();
+}
