052c58dac4f2eecd98091fe991a53b1c51df5f90
P-Wall86 <sisterwall@gmail.com>
2026-01-24 15:51:38 -0300

    Risking everything

--- src/js/cart.js
+++ src/js/cart.js
@@ -30,17 +30,17 @@ function renderCartContents() {
 function cartItemTemplate(item) {
   return `
   <li class="cart-card divider">
-  <button class="remove-item" data-id="${item.Id}" aria-label="Remove ${item.Name} from cart">X</button>
-  <a href="#" class="cart-card__image">
-    <img src="${item.Image}" alt="${item.Name}"/>
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+    <button class="remove-item" data-id="${item.Id}" aria-label="Remove ${item.Name} from cart">X</button>
+    <a href="#" class="cart-card__image">
+      <img src="${item.Image}" alt="${item.Name}"/>
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: ${item.Quantity || 1}</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
 }
 
 loadHeaderFooter();
