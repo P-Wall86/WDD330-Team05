910d05e3ceff86ba60e6978bf37181116c31b136
P-Wall86 <sisterwall@gmail.com>
2026-01-26 21:21:24 -0300

    Ind. Act. 03 - Working

--- src/js/cart.js
+++ src/js/cart.js
@@ -69,14 +69,14 @@ function mergeCartItems(cartItems) {
   <li class="cart-card divider">
     <button class="remove-item" data-id="${item.Id}" aria-label="Remove ${item.Name} from cart">X</button>
     <a href="#" class="cart-card__image">
-      <img src="${item.Image}" alt="${item.Name}"/>
+      <img src="${item.Images?.PrimaryMedium}" alt="${item.Name}"/>
     </a>
     <a href="#">
       <h2 class="card__name">${item.Name}</h2>
     </a>
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
     <p class="cart-card__quantity">qty: ${item.Quantity || 1}</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
+    <p class="cart-card__price">$${item.FinalPrice.toFixed(2)}</p>
   </li>`;
   }
 
