be96bf42a95d565ed13d6c57adf9358ff4ab8038
myrayzsiza <rayzanitah2@gmail.com>
2026-02-12 13:54:06 +0300

    configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   root: "src/",
